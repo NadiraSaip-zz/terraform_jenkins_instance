b7a7e327b467066d6ac04b37629f94576183e909
NadiraSaip <nadiasaip@gmail.com>
2018-11-03 18:57:28 +0000

    changes added

--- vars.tf
+++ vars.tf
@@ -7,8 +7,8 @@ variable "amis" {
   type = "map"
 
   default = {
-   eu-west-1 = "ami-6e28b517"
-   us-west-1 = "ami-65e0e305"
-   us-west-2 = "ami-a042f4d8"
+   eu-west-1 = "ami-0a5e707736615003c"
+   us-west-1 = "ami-01beb64058d271bc4"
+   us-west-2 = "ami-061e7ebbc234015fe"
  }
 }
